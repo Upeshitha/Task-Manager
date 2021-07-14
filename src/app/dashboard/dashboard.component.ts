41aea70394de602fd3c252416192c0ff5253abad
Eranda Upeshitha <upeshitha@gmail.com>
2021-07-14 19:53:38 +0530

    use ngFor
    remove earlier directt calling method and use ngFor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,6 +16,19 @@ export class DashboardComponent implements OnInit {
   CurrentExpenditure: number;
   AvailableFunds: number;
 
+  Clients: string[];
+  Projects: string[];
+  Years: number[] = [];
+  TeamMembersSummary: {
+    Region:string, 
+    TeamMembersCount:number, 
+    TemporarilyUnavailableMembers:number
+  }[];
+  TeamMembers: {
+    Region: string,
+    Members: { ID: number, Name: string, Status: string }[]
+  }[];
+
   constructor(){
     this.Designation = 'Team Leader';
     this.Username = 'Eranda Upeshitha';
@@ -26,10 +39,65 @@ export class DashboardComponent implements OnInit {
     this.ProjectCost = 2113507;
     this.CurrentExpenditure = 96788;
     this.AvailableFunds = 52536;
+
+    this.Clients = [
+      "ABC Infotech Ltd.", "DEF Software Solutions", "GHI Industries"
+    ];
+    this.Projects = [
+      "Project A", "Project B", "Project C", "Project D"
+    ];
+
+    this.TeamMembersSummary = [
+      { "Region": "East", "TeamMembersCount": 20, "TemporarilyUnavailableMembers": 4 },
+      { "Region": "West", "TeamMembersCount": 15, "TemporarilyUnavailableMembers": 8 },
+      { "Region": "South", "TeamMembersCount": 17, "TemporarilyUnavailableMembers": 2 },
+      { "Region": "North", "TeamMembersCount": 15, "TemporarilyUnavailableMembers": 6 }
+    ];
+
+    this.TeamMembers = [
+      {
+        "Region": 'East',
+        "Members": [
+          { ID: 1, Name: 'Lahiru', Status: 'Available' },
+          { ID: 2, Name: 'Kasun', Status: 'Available' },
+          { ID: 3, Name: 'Sithija', Status: 'Busy' },
+          { ID: 4, Name: 'Ruwan', Status: 'Busy' },
+        ],
+      },
+      {
+        Region: 'West',
+        Members: [
+          { ID: 5, Name: 'Nuwan', Status: 'Available' },
+          { ID: 6, Name: 'Akalanka', Status: 'Available' },
+          { ID: 7, Name: 'Hiran', Status: 'Busy' },
+          { ID: 8, Name: 'Asiri', Status: 'Busy' },
+        ],
+      },
+      {
+        Region: 'South',
+        Members: [
+          { ID: 9, Name: 'Janith', Status: 'Available' },
+          { ID: 10, Name: 'Lakshitha', Status: 'Available' },
+          { ID: 11, Name: 'Imalka', Status: 'Busy' },
+          { ID: 12, Name: 'Suraj', Status: 'Busy' },
+        ],
+      },
+      {
+        Region: 'North',
+        Members: [
+          { ID: 13, Name: 'Supun', Status: 'Available' },
+          { ID: 14, Name: 'Sameera', Status: 'Available' },
+          { ID: 15, Name: 'Sumudu', Status: 'Busy' },
+          { ID: 16, Name: 'Randika', Status: 'Busy' },
+        ],
+      }
+    ];
   }
 
   ngOnInit(){ 
-    
+    for(var i=2021; i>=2018; i--){
+      this.Years.push(i);
+    }   
   }
 
 }
