ef05c61d5db9a09d3d3a4c94add7aec7fd89feb3
Eranda Upeshitha <upeshitha@gmail.com>
2021-07-14 16:00:26 +0530

    Create dash board
    Use bootstrap 5 to create dashboard.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  Designation: string;
+  Username: string;
+  NoOfTeamMembers: number;
+  TotalCostOfAllProjects: number;
+  PendingTasks: number;
+  UpComingProjects: number;
+  ProjectCost: number;
+  CurrentExpenditure: number;
+  AvailableFunds: number;
 
-  constructor() { }
+  constructor(){
+    this.Designation = 'Team Leader';
+    this.Username = 'Eranda Upeshitha';
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects = 240;
+    this.PendingTasks = 15;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 96788;
+    this.AvailableFunds = 52536;
+  }
 
-  ngOnInit(): void {
+  ngOnInit(){ 
+    
   }
 
 }
